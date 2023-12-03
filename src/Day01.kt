eb303b53275c91875ebcbcf61f97dd5d8736b2cb
AnirudhPudari <anirudhgoud013@gmail.com>
2023-12-03 10:20:26 +0530

    First day part 1 and part 2 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,68 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    print(part1())
+    print(part2())
 }
+
+val words = listOf("one", "two", "three", "four", "five", "six", "seven", "eight", "nine")
+
+fun part1() {
+    val testInput = readInput("day01")
+
+    println(testInput.sumOf { line: String ->
+        line.first { it.isDigit() }.digitToInt() * 10 + line.last { it.isDigit() }.digitToInt()
+    })
+}
+
+
+fun part2() {
+    val lines = readInput("day01_part_2")
+    println(lines.sumOf { line: String ->
+        getNumber(line, StartFrom.BEGINNING) * 10 + getNumber(line, StartFrom.END)
+    })
+}
+
+fun getNumber(line: String, beginning: StartFrom): Int {
+
+    val indices = when (beginning) {
+        StartFrom.BEGINNING -> line.indices
+        StartFrom.END -> line.lastIndex downTo 0
+    }
+
+    for (idx in indices) {
+        line[idx].digitToIntOrNull()?.let { return it }
+        for ((wordIndex, word) in words.withIndex()) {
+            if (line.substring(idx).startsWith(word)) {
+                return (wordIndex + 1)
+            }
+        }
+    }
+    error("Unreachable")
+}
+
+fun calculateSumOfCalibrationValues(calibrationDocument: String): Int {
+    // Split the document into lines
+    val lines = calibrationDocument.lines()
+
+    // Extract and sum the calibration values
+    val sum = lines.map { line ->
+        // Filter out non-digit characters
+        val digits = line.filter { it.isDigit() }
+
+        // Extract the first and last digits
+        val firstDigit = digits.firstOrNull()
+        val lastDigit = digits.lastOrNull()
+
+        // Calculate the calibration value
+        val calibrationValue = if (firstDigit != null && lastDigit != null) {
+            "$firstDigit$lastDigit".toInt()
+        } else {
+            0
+        }
+
+        calibrationValue
+    }.sum()
+
+    return sum
+}
+
+enum class StartFrom { BEGINNING, END }
